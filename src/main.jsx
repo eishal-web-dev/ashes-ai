a2c285781d5da9fb69b0ccb0942e999299c77908
Eishal <eishal.cecos@gmail.com>
2026-08-09 18:07:12 +0500

    load app-wide UI polish last

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './ui-refresh.css';
+import './app-polish.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
