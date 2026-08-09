4b22d912168afa366500d5385b27901364ed3f66
Eishal <eishal.cecos@gmail.com>
2026-08-09 19:38:36 +0500

    Load dedicated super admin UI last

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './app-polish.css';
 import './dashboard-refresh.css';
 import './website-refresh.css';
 import './onboarding.css';
+import './superadmin-refresh.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
