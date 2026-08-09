2dc21388f78cc1bf47a4745edfcaf52397b6c97e
Eishal <eishal.cecos@gmail.com>
2026-08-09 20:00:44 +0500

    Wire profile theme galleries into business and admin UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import ProfileThemeEnhancer from './ProfileThemeEnhancer';
 import './styles.css';
 import './ui-refresh.css';
 import './app-polish.css';
@@ -9,9 +10,11 @@ import './website-refresh.css';
 import './onboarding.css';
 import './superadmin-refresh.css';
 import './retail-warranty.css';
+import './profile-themes.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ProfileThemeEnhancer />
   </React.StrictMode>
 );
