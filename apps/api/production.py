a70e4548fc1a2e0e4c4b650fef725188686b3632
Eishal <eishal.cecos@gmail.com>
2026-08-09 22:08:52 +0500

    Register commerce source routes

--- apps/api/production.py
+++ apps/api/production.py
@@ -20,6 +20,7 @@ import apps.api.manual_payments  # noqa: F401
 import apps.api.retail_products  # noqa: F401
 import apps.api.table_qr_routes  # noqa: F401
 import apps.api.order_operations  # noqa: F401
+import apps.api.commerce_sources  # noqa: F401
 from apps.api.mongo_db import database
 
 
