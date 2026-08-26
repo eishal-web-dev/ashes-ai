23eb91c6009cefcfdbf42d30f0633ea166ca9f26
Eishal <eishal.cecos@gmail.com>
2026-08-26 10:20:08 -0700

    Load Shopify multi-view recovery middleware

--- apps/api/production.py
+++ apps/api/production.py
@@ -35,6 +35,7 @@ import apps.api.shopify_viewer_paths  # noqa: F401
 import apps.api.shopify_asset_startup  # noqa: F401
 import apps.api.shopify_publish_click_fix  # noqa: F401
 import apps.api.shopify_multiview_ui  # noqa: F401
+import apps.api.shopify_multiview_recovery  # noqa: F401
 import apps.api.shopify_session_auth  # noqa: F401
 from apps.api.mongo_db import database
 
